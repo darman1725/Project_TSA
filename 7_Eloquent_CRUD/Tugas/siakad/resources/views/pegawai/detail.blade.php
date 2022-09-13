3b9ee36606ef059fb6c0b9c0b297bfb9b00de621
darman1725 <darmansaragih1701@gmail.com>
2022-09-13 20:40:35 +0700

    Tugas - Eloquent dan CRUD

---
+++ 7_Eloquent_CRUD/Tugas/siakad/resources/views/pegawai/detail.blade.php
@@ -0,0 +1,25 @@
+@extends('pegawai.layout')
+
+@section('content')
+<div class="container mt-5">
+    <div class="row justify-content-center align-items-center">
+        <div class="card" style="width: 24rem;">
+            <div class="card-header">
+                Detail Pegawai - BPS Kota Malang
+            </div>
+            <div class="card-body">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><b>Nomor Pegawai: </b>{{$Pegawai->nomor_pegawai}}</li>
+                    <li class="list-group-item"><b>Nama: </b>{{$Pegawai->nama}}</li>
+                    <li class="list-group-item"><b>Divisi: </b>{{$Pegawai->divisi}}</li>
+                    <li class="list-group-item"><b>Jabatan: </b>{{$Pegawai->jabatan}}</li>
+                    <li class="list-group-item"><b>Email: </b>{{$Pegawai->email}}</li>
+                    <li class="list-group-item"><b>Alamat: </b>{{$Pegawai->alamat}}</li>
+                    <li class="list-group-item"><b>Tanggal Lahir: </b>{{$Pegawai->tanggal_lahir}}</li>
+                </ul>
+            </div>
+            <a class="btn btn-success mt-3" href="{{ route('pegawai.index') }}">Kembali</a>
+        </div>
+    </div>
+</div>
+@endsection
