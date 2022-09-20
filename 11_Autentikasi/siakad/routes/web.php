e484b364b2c9b9aed29e56d6634f3b78120156e7
darman1725 <darmansaragih1701@gmail.com>
2022-09-20 11:20:33 +0700

    Praktikum 2 : Add login as Administrator

---
+++ 11_Autentikasi/siakad/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PegawaiController;
+use Illuminate\Http\Request;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route untuk pegawai
+Route::resource('pegawai', PegawaiController::class);
+
+// Route untuk cetak data pegawai
+Route::get('/downloadPDF', [PegawaiController::class, 'downloadPDF'])->name('downloadPDF');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
